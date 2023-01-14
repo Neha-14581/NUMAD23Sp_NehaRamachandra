16fa7895140e6e5b627a3272cede94e8d1a2019a
DESKTOP-C0NKLQ5\18573 <ramachandra.n@northeastern.edu>
2023-01-15 00:35:04 +0530

    Initial commit after git linked to Android Studio

---
+++ app/src/main/java/edu/northeastern/numad23sp_neharamachandra/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.northeastern.numad23sp_neharamachandra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //When the AboutMe button is clicked, the following thing should take place.
+
+        Button btn1 = findViewById(R.id.buttonAboutMe);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Neha Ramachandra\n ramachandra.n@northeastern.edu", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+
+    }
+}
